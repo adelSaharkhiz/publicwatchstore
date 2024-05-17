f76efe692fee60274f06c298ac3c1569f176dfd0
adelSaharkhiz <msaharkhiz1377@gmail.com>
2024-05-17 20:22:25 +0330

    212

--- lib/widgets/app_slider.dart
+++ lib/widgets/app_slider.dart
@@ -9,10 +9,17 @@ List<String> imgList = [
   "https://static.digiato.com/digiato/2024/05/9uYrn4LCZ286kVwsQ7jNNW-1200-80-910x600.jpg"
 ];
 
-class AppSlider extends StatelessWidget {
-  AppSlider({
+class AppSlider extends StatefulWidget {
+  const AppSlider({
     super.key,
   });
+
+  @override
+  State<AppSlider> createState() => _AppSliderState();
+}
+
+class _AppSliderState extends State<AppSlider> {
+  final CarouselController _controller = CarouselController();
 
   final List<Widget> items = imgList
       .map((e) => Padding(
@@ -23,19 +30,48 @@ class AppSlider extends StatelessWidget {
           ))
       .toList();
 
+  int _currunt = 0;
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.sizeOf(context);
-    return Container(
-      margin: const EdgeInsets.all(AppDimens.medium),
-      height: size.height * .29,
-      width: double.infinity,
-      child: CarouselSlider(
-          items: items,
-          options: CarouselOptions(
-            autoPlay: true,
-            onPageChanged: (index, reason) {},
-          )),
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.all(AppDimens.medium),
+          height: size.height * .29,
+          width: double.infinity,
+          child: CarouselSlider(
+              carouselController: _controller,
+              items: items,
+              options: CarouselOptions(
+                autoPlay: true,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _currunt = index;
+                  });
+                },
+              )),
+        ),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: imgList
+                .asMap()
+                .entries
+                .map((e) => GestureDetector(
+                      onTap: () => _controller.animateToPage(e.key),
+                      child: Container(
+                        margin: const EdgeInsets.all(AppDimens.small * .5),
+                        height: 12,
+                        width: 12,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color:
+                                _currunt == e.key ? Colors.black : Colors.grey),
+                      ),
+                    ))
+                .toList())
+      ],
     );
   }
 }
